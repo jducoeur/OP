043c5e01ba3cf1b164575e923ebcb1a7214e04e7
anna bradley <arbradley@gmail.com>
2013-02-16 15:55:40 -0500

    more edits woot

--- op/html/award_by_discpline.php
+++ op/html/award_by_discpline.php
@@ -1,155 +1,40 @@
-<table width="<?php echo $pagewidth; ?>" align="center" cellpadding="5" cellspacing="0" border="1" summary="Awards by Category">
-   <caption class="title2">Orders and Awards by Discipline</caption>
+<br/><br/>
+<table width="<?php echo $pagewidth; ?>" align="center" cellpadding="5" cellspacing="0" border="1" summary="Table for layout">
    <tr>
-      <th class="title">DISCIPLINE</th>
-      <th class="title">Peerage Orders<br/>(carry Patent of Arms)</th>
-      <th class="title" colspan="2">Orders of High Merit<br/>(carry Grant of Arms)</th>
-      <th class="title">Orders of Merit<br/>(carry Award of Arms)</th>
-      <th class="title">Awards/Orders<br/>(non-armigerous)</th>
-      <th class="title">Youth Awards/Orders<br/>(non-armigerous)</th>
+      <th class="title">Kingdom Honors</th>
    </tr>
    <tr>
-      <th class="title">Generic</th>
-      <td class="datacenter">(<a href="<?php echo $HOME_DIR; ?>op_award.php?award_id=<?php echo $POA; ?>">Patent of Arms</a>)</td>
-      <td class="datacenter" colspan="2"><a href="<?php echo $HOME_DIR; ?>op_award.php?award_id=<?php echo $GOA; ?>">Grant of Arms</a></td>
-      <td class="datacenter"><a href="<?php echo $HOME_DIR; ?>op_award.php?award_id=<?php echo $AOA; ?>">Award of Arms</a></td>
-      <td class="datacenter"><a href="<?php echo $HOME_DIR; ?>op_award.php?award_id=<?php echo $AUG; ?>">Augmentation of Arms</a></td>
-      <td class="datacenter"><img src="<?php echo $AWARD_IMAGE_DIR; ?>sea_urchin.gif" width="40" height="40" alt="Award of the Sea Urchin" border="0" align="middle"/> <a href="<?php echo $HOME_DIR; ?>op_award.php?award_id=<?php echo $SEA_URCHIN_ID; ?>">Sea Urchin</a>
+    <!--  <td class="datacenter">
+      <table width="100%" border="0" align="center" cellpadding="5" cellspacing="0" summary="Table for layout">
+         <tr>
+            <td class="datacenter" width="100%" valign="top">
+            <img src="<?php echo $AWARD_IMAGE_DIR; ?>saint_aidan.gif" width="40" height="40" alt="Order of Saint Aidan" border="0" align="middle"/> <a href="<?php echo $HOME_DIR; ?>op_award.php?award_id=<?php echo $ST_AIDAN; ?>">Company of Sergeants of Saint Aidan</a>
             <br/>
-            Youth are awarded the Sea Urchin for contributions to the Kingdom in service, martial activities, and/or arts and sciences.
-      </td>
-   </tr>
-   <tr>
-      <th class="title">Arts &amp; Sciences</th>
-      <td class="datacenter"><img src="<?php echo $AWARD_IMAGE_DIR; ?>laurel.gif" width="40" height="40" alt="Order of the Laurel" border="0" align="middle"/> <a href="<?php echo $HOME_DIR; ?>op_award.php?award_id=<?php echo $LAUREL; ?>">Laurels</a>
-            <br/>
-            Bestowed Patent: One becomes a Laurel through excellence in the arts and sciences.</td>
-      <td class="datacenter" colspan="2"><img src="<?php echo $AWARD_IMAGE_DIR; ?>pearl.gif" width="40" height="40" alt="Order of the Pearl" border="0" align="middle"/> <a href="<?php echo $HOME_DIR; ?>op_award.php?award_group_id=<?php echo $PEARL_GROUP; ?>">Pearl</a>
-            <br/>
-            Grant Level: One becomes a Pearl through teaching and excellence in the arts and sciences.</td>
-      <td class="datacenter"><img src="<?php echo $AWARD_IMAGE_DIR; ?>coral_branch.gif" width="40" height="40" alt="Order of the Coral Branch" border="0" align="middle"/> <a href="<?php echo $HOME_DIR; ?>op_award.php?award_id=<?php echo $CORAL_BRANCH_ID; ?>">Coral Branch</a>
-            <br/>
-            AoA Level: Non-polling order given to those who excel in the arts and sciences in the kingdom.</td>
-      <td class="datacenter"><img src="<?php echo $AWARD_IMAGE_DIR; ?>silver_nautilus.gif" width="40" height="40" alt="Award of the Silver Nautilus" border="0" align="middle"/> <a href="<?php echo $HOME_DIR; ?>op_award.php?award_id=<?php echo $SILVER_NAUTILUS_ID; ?>">Silver Nautilus</a>
-            <br/>
-            One is awarded the Silver Nautilus in recognition of skill in the arts and sciences.</td>
-      <td class="datacenter"><img src="<?php echo $AWARD_IMAGE_DIR; ?>alcyon.gif" width="40" height="40" alt="Order of the Alcyon" border="0" align="middle"/> <a href="<?php echo $HOME_DIR; ?>op_award.php?award_id=<?php echo $ALCYON_ID; ?>">Alcyon</a>
-            <br/>
-            Youth are awarded the Alcyon for excellence in arts and sciences.</td>
-   </tr>
-   <tr>
-      <th class="title">Service</th>
-      <td class="datacenter"><img src="<?php echo $AWARD_IMAGE_DIR; ?>pelican.gif" width="40" height="40" alt="Order of the Pelican" border="0" align="middle"/> <a href="<?php echo $HOME_DIR; ?>op_award.php?award_id=<?php echo $PELICAN; ?>">Pelicans</a>
-            <br/>
-            Bestowed Patent: One becomes a Pelican through excellence in service.</td>
-      <td class="datacenter" colspan="2"><img src="<?php echo $AWARD_IMAGE_DIR; ?>golden_dolphin.gif" width="40" height="40" alt="Order of the Golden Dolphin" border="0" align="middle"/> <a href="<?php echo $HOME_DIR; ?>op_award.php?award_group_id=<?php echo $DOLPHIN_GROUP; ?>">Golden Dolphin</a>
-            <br/>
-            Grant Level: One becomes a Golden Dolphin through excellence in service and leadership.</td>
-      <td class="datacenter"><img src="<?php echo $AWARD_IMAGE_DIR; ?>opal.gif" width="40" height="40" alt="Order of the Opal" border="0" align="middle"/> <a href="<?php echo $HOME_DIR; ?>op_award.php?award_id=<?php echo $OPAL_ID; ?>">Opal</a>
-            <br/>
-            AoA Level: Non-polling order given to those who excel in service to the kingdom.</td>
-      <td class="datacenter"><img src="<?php echo $AWARD_IMAGE_DIR; ?>fountain.gif" width="40" height="40" alt="Award of the Fountain" border="0" align="middle"/> <a href="<?php echo $HOME_DIR; ?>op_award.php?award_id=<?php echo $FOUNTAIN_ID; ?>">Fountain</a>
-            <br/>
-            One is awarded the Fountain in recognition of service to the kingdom.
-            <br/><br/>
-            <img src="<?php echo $AWARD_IMAGE_DIR; ?>herring.gif" width="40" height="40" alt="Award of the Herring" border="0" align="middle"/> <a href="<?php echo $HOME_DIR; ?>op_award.php?award_id=<?php echo $HERRING_ID; ?>">Herring</a>
-            <br/>
-            One is awarded the Herring for extraordinary achievement as an autocrat.
-      </td>
-      <td class="datacenter"><img src="<?php echo $AWARD_IMAGE_DIR; ?>hippocampus.gif" width="40" height="40" alt="Order of the Hippocampus" border="0" align="middle"/> <a href="<?php echo $HOME_DIR; ?>op_award.php?award_id=<?php echo $HIPPOCAMPUS_ID; ?>">Hippocampus</a>
-            <br/>
-            Youth are awarded the Hippocampus for excellence in service and leadership.
-   </tr>
-   <tr>
-      <th class="title">Armored Combat</th>
-      <td class="datacenter"><img src="<?php echo $AWARD_IMAGE_DIR; ?>knight.gif" width="40" height="40" alt="Order of the Chivalry (Knighthood)" border="0" align="middle"/> 
-            <img src="<?php echo $AWARD_IMAGE_DIR; ?>m_at_arms.gif" width="40" height="40" alt="Order of the Chivalry (Masters/Mistresses at Arms)" border="0" align="middle"/> 
-            <a href="<?php echo $HOME_DIR; ?>op_award.php?award_group_id=<?php echo $CHIVALRY_GROUP; ?>">Chivalry</a>
-            <br/>
-            Bestowed Patent: One becomes a Knight or Master/Mistress at Arms through martial prowess on the field.</td>
-      <td class="datacenter"><img src="<?php echo $AWARD_IMAGE_DIR; ?>kraken.gif" width="40" height="40" alt="Order of the Kraken" border="0" align="middle"/> <a href="<?php echo $HOME_DIR; ?>op_award.php?award_group_id=<?php echo $KRAKEN_GROUP; ?>">Kraken</a>
-            <br/>
-            Grant Level: One is awarded the Kraken for excellence in armored combat (non-belted fighters).</td>
-      <td class="datacenter" rowspan="3"><img src="<?php echo $AWARD_IMAGE_DIR; ?>sea_stag.gif" width="40" height="40" alt="Order of the Sea Stag" border="0" align="middle"/> <a href="<?php echo $HOME_DIR; ?>op_award.php?award_group_id=<?php echo $SEA_STAG_GROUP; ?>">Sea Stag</a>
-            <br/>
-            Grant Level: One becomes a Sea Stag through excellence in teaching armored combat, rapier combat, combat and target archery, thrown weapons, siege warfare, or youth combat.</td>
-      <td class="datacenter"><img src="<?php echo $AWARD_IMAGE_DIR; ?>silver_osprey.gif" width="40" height="40" alt="Order of the Silver Osprey" border="0" align="middle"/> <a href="<?php echo $HOME_DIR; ?>op_award.php?award_id=<?php echo $SILVER_OSPREY_ID; ?>">Silver Osprey</a>
-            <br/>
-            AoA Level: Non-polling order given to those who excel in fighting in the kingdom.</td>
-      <td class="datacenter" rowspan="3"><img src="<?php echo $AWARD_IMAGE_DIR; ?>sharks_tooth.gif" width="40" height="40" alt="Award of the Shark's Tooth" border="0" align="middle"/> <a href="<?php echo $HOME_DIR; ?>op_award.php?award_id=<?php echo $SHARKS_TOOTH_ID; ?>">Shark's Tooth</a>
-            <br/>
-            One is awarded a Shark's Tooth for an act of bravery on the field.
-            <br/>
-            <br/>
-            <img src="<?php echo $AWARD_IMAGE_DIR; ?>vexillum_atlantiae.gif" width="40" height="40" alt="Award of the Vexillum Atlantiae" border="0" align="middle"/> <a href="<?php echo $HOME_DIR; ?>op_award.php?award_id=<?php echo $VEXILLUM_ID; ?>">Vexillum Atlantiae</a>
-            <br/>
-            A group is awarded the Vexillum Atlantiae for an act of bravery on the field.
+            This is a fighting order where members are selected by existing order members and brought in by the King.
             </td>
-      <td class="datacenter" rowspan="3"><img src="<?php echo $AWARD_IMAGE_DIR; ?>sea_tyger.gif" width="40" height="40" alt="Order of the Sea Tyger" border="0" align="middle"/> <a href="<?php echo $HOME_DIR; ?>op_award.php?award_id=<?php echo $SEA_TIGER_ID; ?>">Sea Tyger</a>
-            <br/>
-            Youth are awarded the Sea Tyger for excellence in youth combat.</td>
+         </tr>
+      </table>
+      </td> --> 
    </tr>
    <tr>
-      <th class="title">Rapier Combat</th>
-      <td class="datacenter">&nbsp;</td>
-      <td class="datacenter"><img src="<?php echo $AWARD_IMAGE_DIR; ?>white_scarf.gif" width="40" height="40" alt="Order of the White Scarf" border="0" align="middle"/> <a href="<?php echo $HOME_DIR; ?>op_award.php?award_id=<?php echo $WHITE_SCARF_ID; ?>">White Scarf</a>
-            <br/>
-            Grant Level: One receives a White Scarf for prowess in rapier combat.</td>
-      <td class="datacenter"><img src="<?php echo $AWARD_IMAGE_DIR; ?>sea_dragon.gif" width="40" height="40" alt="Order of the Sea Dragon" border="0" align="middle"/> <a href="<?php echo $HOME_DIR; ?>op_award.php?award_id=<?php echo $SEA_DRAGON_ID; ?>">Sea Dragon</a>
-            <br/>
-            AoA Level: Non-polling order given to those who excel in Rapier fighting in the kingdom.</td>
+      <th class="title">Closed Orders</th>
    </tr>
-   <tr>
-      <th class="title">Archery</th>
-      <td class="datacenter">&nbsp;</td>
-      <td class="datacenter"><img src="<?php echo $AWARD_IMAGE_DIR; ?>yew_bow.gif" width="40" height="40" alt="Order of the Yew Bow" border="0" align="middle"/> <a href="<?php echo $HOME_DIR; ?>op_award.php?award_group_id=<?php echo $YEW_BOW_GROUP; ?>">Yew Bow</a>
+   <tr><!--
+      <td class="datacenter">
+      <table width="100%" border="0" align="center" cellpadding="5" cellspacing="0" summary="Table for layout">
+         <tr>
+            <td class="datacenter" width="50%" valign="top">
+            <a href="<?php echo $HOME_DIR; ?>op_award.php?award_id=<?php echo $SILVER_NEEDLE_ID; ?>">Order of the Silver Needle</a>
             <br/>
-            Grant Level: One is awarded the Yew Bow for excellence with bow and arrow (both target and combat), thrown weapons, or siegecraft.</td>
-      <td class="datacenter"><img src="<?php echo $AWARD_IMAGE_DIR; ?>kings_missiliers.gif" width="40" height="40" alt="Order of the King's Missiliers" border="0" align="middle"/> <a href="<?php echo $HOME_DIR; ?>op_award.php?award_id=<?php echo $MISSILIERS_ID; ?>">King's Missiliers</a>
+            Closed order for costuming.
+            </td>
+            <td class="datacenter" width="50%" valign="top">
+            <a href="<?php echo $HOME_DIR; ?>op_award.php?award_id=<?php echo $ACADEMIE_DESPEE_ID; ?>">Order of l'Academie d'Espee</a>
             <br/>
-            AoA Level: Non-polling order given to those who excel in archery (both target and combat), thrown weapons, or siegecraft in the kingdom.</td>
-   </tr>
-   <tr>
-      <th class="title">Equestrian</th>
-      <td class="datacenter">&nbsp;</td>
-      <td class="datacenter" colspan="2">&nbsp;</td>
-      <td class="datacenter"><img src="<?php echo $AWARD_IMAGE_DIR; ?>quintain.gif" width="40" height="40" alt="Order of the Quintain" border="0" align="middle"/> <a href="<?php echo $HOME_DIR; ?>op_award.php?award_id=<?php echo $LANCERS_ID; ?>">Quintain</a>
-            <br/>
-            AoA Level: Non-polling order given to those who excel in equestrian in the kingdom.</td>
-      <td class="datacenter">&nbsp;</td>
-      <td class="datacenter">&nbsp;</td>
-   </tr>
-   <tr>
-      <th class="title">Courtesy</th>
-      <td class="datacenter">&nbsp;</td>
-      <td class="datacenter" colspan="2">&nbsp;</td>
-      <td class="datacenter">&nbsp;</td>
-      <td class="datacenter"><img src="<?php echo $AWARD_IMAGE_DIR; ?>qoc.gif" width="40" height="40" alt="Queen's Order of Courtesy" border="0" align="middle"/> <a href="<?php echo $HOME_DIR; ?>op_award.php?award_id=<?php echo $QOC_ID; ?>">Queen's Order of Courtesy</a>
-            <br/>
-            Given by the Queen to those subjects she deems worthy by reason of their consistently exemplary courtesy to subjects of all ranks in the Realm and in the Society at large.</td>
-      <td class="datacenter"><img src="<?php echo $AWARD_IMAGE_DIR; ?>arielle.gif" width="40" height="40" alt="Arielle" border="0" align="middle"/> <a href="<?php echo $HOME_DIR; ?>op_award.php?award_id=<?php echo $ARIELLE_ID; ?>">Arielle</a>
-            <br/>
-            Youth are awarded the Arielle for acts of courtesy.</td>
-   </tr>
-   <tr>
-      <th class="title">TRM's Discretion</th>
-      <td class="datacenter">&nbsp;</td>
-      <td class="datacenter" colspan="2">&nbsp;</td>
-      <td class="datacenter"><img src="<?php echo $AWARD_IMAGE_DIR; ?>cbaron.gif" width="65" height="30" alt="Court Baronage" border="0" align="middle"/> <a href="<?php echo $HOME_DIR; ?>op_award.php?award_group_id=<?php echo $COURT_BARONAGE_GROUP; ?>">Court Baronage</a></td>
-      <td class="datacenter"><img src="<?php echo $AWARD_IMAGE_DIR; ?>nonpareil.gif" width="40" height="40" alt="Order of the Nonpareil" border="0" align="middle"/> <a href="<?php echo $HOME_DIR; ?>op_award.php?award_id=<?php echo $NONPAREIL_ID; ?>">Order of the Nonpareil</a>
-            <br/>
-            One is awarded the Nonpareil for personifying what it is to be an Atlantian. This award may only be given once per reign.
-            <br/><br/>
-            <img src="<?php echo $AWARD_IMAGE_DIR; ?>kae.gif" width="40" height="40" alt="King's Award of Excellence" border="0" align="middle"/> <a href="<?php echo $HOME_DIR; ?>op_award.php?award_id=<?php echo $KAE_ID; ?>">King's Award of Excellence</a>
-            <br/>
-            One is awarded the KAE for excellence in service to the King and kingdom.
-            <br/><br/>
-            <img src="<?php echo $AWARD_IMAGE_DIR; ?>undine.gif" width="40" height="40" alt="Award of the Undine" border="0" align="middle"/> <a href="<?php echo $HOME_DIR; ?>op_award.php?award_id=<?php echo $UNDINE_ID; ?>">Undine</a>
-            <br/>
-            One is awarded the Undine for excellence in service to the Queen.
-            <br/><br/>
-            <a href="<?php echo $HOME_DIR; ?>op_award.php?award_id=<?php echo $SUPPORTERS_ID; ?>">Supporters</a>
-      </td>
-      <td class="datacenter">&nbsp;</td>
+            Closed order for rapier.  This order was replaced by the Order of the White Scarf.
+            </td> 
+         </tr>
+      </table>
+      </td>-->
    </tr>
 </table>
