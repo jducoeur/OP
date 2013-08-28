b7cef31bcfbba7f6975d0db7bbc1f9374fdec1e6
anna bradley <arbradley@gmail.com>
2013-08-28 18:52:29 -0400

    inex

--- op/html/index.php
+++ op/html/index.php
@@ -10,10 +10,10 @@ if (isset($printable) && $printable == 1)
 <p align="center">
 The <?php echo $KINGDOM_ADJ; ?> Order of Precedence includes all the accolades bestowed upon the populace of <?php echo $KINGDOM_NAME; ?> that have been reported to the Clerk of Precedence.
 <br/><br/>
-Please send all <b>Royal</b> <a href="court_reports.php">court reports</a> to <b><?php echo $COURT_REP_EMAIL; ?></b>: This alias emails the Clerk of Precedence, the Principal Herald, 
+Please send all <b>Royal</b> <a href="http://eastkingdom.org/heraldry/">court reports</a> to <b><?php echo $COURT_REP_EMAIL; ?></b>: This alias emails the Clerk of Precedence, the Principal Herald, 
 the Clerk Signet, the Backlog Deputy and the presiding Crowns.
 <br/><br/>
-Please send all <b>Baronial</b> <a href="court_reports.php">court reports</a> to <b><?php echo $COURT_REP_EMAIL; ?></b>: This alias emails the Clerk of Precedence, the Principal Herald, 
+Please send all <b>Baronial</b> <a href="http://eastkingdom.org/heraldry/">court reports</a> to <b><?php echo $COURT_REP_EMAIL; ?></b>: This alias emails the Clerk of Precedence, the Principal Herald, 
 the Clerk Signet and the Backlog Deputy.  Baronial court reports should also be reported to the presiding coronets and the baronial herald (if not the one conducting court).
 <br/><br/>
 For updates and corrections, please use the <a href="corrections.php">Corrections form</a>. 
